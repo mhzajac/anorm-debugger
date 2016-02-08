278cc1b4d511ed6290ba488041959c212c2ff4c9
Michael Zajac <mzajac@jaroop.com>
2016-02-08 05:19:06 +0000

    Added a ResultSetDebugger test

--- src/test/scala/com/jaroop/anorm/AnormFixtures.scala
+++ src/test/scala/com/jaroop/anorm/AnormFixtures.scala
@@ -4,15 +4,20 @@ case class ResultRow(metaData: MetaData, data: List[Any]) extends Row
 
 object Results {
 
-	val metaData = ResultRow(
-		MetaData(
-			List(
-				MetaDataItem(ColumnName("bars.id", Some("id")), false, "java.lang.Integer"),
-				MetaDataItem(ColumnName("bars.baz", Some("baz")), false, "java.lang.Boolean"),
-				MetaDataItem(ColumnName("bars.buzz", Some("buzz")), true, "java.lang.Integer")
-			)
-		),
-		List(1L, true, null)
+	val barMeta = MetaData(
+		List(
+			MetaDataItem(ColumnName("bars.id", Some("id")), false, "java.lang.Integer"),
+			MetaDataItem(ColumnName("bars.baz", Some("baz")), false, "java.lang.Boolean"),
+			MetaDataItem(ColumnName("bars.buzz", Some("buzz")), true, "java.lang.Integer")
+		)
+	)
+
+	val singleBarRow = ResultRow(barMeta, List(1L, true, null))
+
+	val listBarMeta = Stream(
+		ResultRow(barMeta, List(1L, true, null)),
+		ResultRow(barMeta, List(2L, true, "a")),
+		ResultRow(barMeta, List(3L, false, "b"))
 	)
 
 }
--- src/test/scala/com/jaroop/anorm/MacroSpec.scala
+++ src/test/scala/com/jaroop/anorm/MacroSpec.scala
@@ -18,16 +18,18 @@ class Interceptor(out: OutputStream) extends PrintStream(out, true) {
 
 }
 
-class MacroSpec extends FlatSpec with Matchers {
+class MacroSpec extends FlatSpec with Matchers with BeforeAndAfterEach {
 
     val interceptor = new Interceptor(Console.out)
 
     Console.setOut(interceptor)
 
-    "The parser debugger macro" should "transform a simple parser into a debugger and log the results" in {
+    override def beforeEach(): Unit = {
         interceptor.reset()
+    }
 
-        @debug.parser("Foo.parser")
+    "The parser debugger macro" should "transform a simple parser into a debugger and log the results" in {
+        @debug.parser("Bar.parser")
         val parser: RowParser[Bar] = {
             get[Long]("bars.id") ~
             get[Boolean]("bars.baz") ~
@@ -38,14 +40,58 @@ class MacroSpec extends FlatSpec with Matchers {
 
         val expected = RowParserDebugger.star +
             RowParserDebugger.dash +
-            "RowParser: Foo.parser" +
+            "RowParser: Bar.parser" +
             RowParserDebugger.dash +
             """get[Long]("bars.id") : Success(1)""" +
             """get[Boolean]("bars.baz") : Success(true)""" +
             """get[Option[String]]("bars.buzz") : Success(None)""" +
             RowParserDebugger.star
 
-        parser(Results.metaData)
+        parser(Results.singleBarRow)
+
+        interceptor.result should equal(expected)
+    }
+
+    it should "log a List ResultSetParser" in {
+        @debug.parser("Bar.parser")
+        val parser: RowParser[Bar] = {
+            get[Long]("bars.id") ~
+            get[Boolean]("bars.baz") ~
+            get[Option[String]]("bars.buzz") map { case id~baz~buzz =>
+                Bar(id, baz, buzz)
+            }
+        }
+
+        val expected = RowParserDebugger.star +
+        RowParserDebugger.dash +
+        "RowParser: Bar.parser" +
+        RowParserDebugger.dash +
+        """get[Long]("bars.id") : Success(1)""" +
+        """get[Boolean]("bars.baz") : Success(true)""" +
+        """get[Option[String]]("bars.buzz") : Success(None)""" +
+        RowParserDebugger.star +
+        RowParserDebugger.star +
+        RowParserDebugger.dash +
+        "RowParser: Bar.parser" +
+        RowParserDebugger.dash +
+        """get[Long]("bars.id") : Success(2)""" +
+        """get[Boolean]("bars.baz") : Success(true)""" +
+        """get[Option[String]]("bars.buzz") : Success(Some(a))""" +
+        RowParserDebugger.star +
+        RowParserDebugger.star +
+        RowParserDebugger.dash +
+        "RowParser: Bar.parser" +
+        RowParserDebugger.dash +
+        """get[Long]("bars.id") : Success(3)""" +
+        """get[Boolean]("bars.baz") : Success(false)""" +
+        """get[Option[String]]("bars.buzz") : Success(Some(b))""" +
+        RowParserDebugger.star +
+        RowParserDebugger.star +
+        """Bar.parser.* rows returned: 3""" +
+        """Bar.parser.* rows parsed: 3""" +
+        RowParserDebugger.star
+
+        parser.*(Results.listBarMeta)
 
         interceptor.result should equal(expected)
     }
